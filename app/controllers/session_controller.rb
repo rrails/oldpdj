ee01c5ca56a749392f67836265ec0209edbd1e36
RRails <sanjita_quinn@hotmail.com>
2013-08-03 17:05:52 +1000

    Fixed logoff

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,21 +1,22 @@
 class SessionController < ApplicationController
 
-def new
-end
-
-def create
-  user = User.where(:name => params[:name]).first
-  if user.present? && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to(root_path)
-  else
-    redirect_to(login_path)
+  def new
   end
-end
 
-def destroy
-  session[:user_id] = nil
-  redirect_to(root_path)
-end
+  def create
+    user = User.where(:name => params[:name]).first
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to(root_path)
+    else
+      redirect_to(signin_path)
+    end
+  end
+
+  def destroy
+    binding.pry
+    session[:user_id] = nil
+    redirect_to(root_path)
+  end
 
 end
